6bffbf85a26c8188729a6a6378f50279f5ec785c
JheanParedes <70239105+AntonyParedes@users.noreply.github.com>
2024-07-20 17:13:56 -0500

    update8

--- src/components/Phrase.jsx
+++ src/components/Phrase.jsx
@@ -1,6 +1,6 @@
 const Phrase = ({ phraseSelected }) => {
   return (
-    <p className="font-mono mb-6  text-gray-700 top-auto left-auto ">
+    <p className="font-mono mb-6  text-gray-700 overflow-hidden">
       {phraseSelected.phrase}
     </p>
   );
